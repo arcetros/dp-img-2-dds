0e100db7bf8731e054e08ccb5e4380cb736febd6
arcetros <75669794+arcetros@users.noreply.github.com>
2023-03-14 15:25:57 +0800

    exclude .gitignore from getting read

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-import { getMtaPath } from "./mta-installation";
 import fs from "fs-extra";
 import * as path from "path";
+import { getMtaPath } from "./mta-installation";
 import ImageConverter from "./image-converter";
 
 const FOLDER_PATH = path.join(process.cwd(), "images");
@@ -9,7 +9,11 @@ const TEXCONV_PATH = path.join(process.cwd(), "texconv.exe");
 const converter = new ImageConverter(FOLDER_PATH, FOLDER_PATH_OUTPUT, TEXCONV_PATH);
 
 (async function (): Promise<void> {
-    const inputFiles = fs.readdirSync(FOLDER_PATH);
+    const dirents = fs.readdirSync(FOLDER_PATH, { withFileTypes: true });
+    //ignores .gitignore
+    const inputFiles = dirents
+        .filter(dirent => dirent.isFile() && dirent.name !== ".gitignore")
+        .map(dirent => dirent.name);
     try {
         await getMtaPath();
         fs.ensureDirSync(FOLDER_PATH_OUTPUT);
