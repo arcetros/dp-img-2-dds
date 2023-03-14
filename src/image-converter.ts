3b10bf5146f98c09398b8b6ebf17fbfc373e94b6
arcetros <75669794+arcetros@users.noreply.github.com>
2023-03-14 14:02:55 +0800

    use class for image-converter

--- src/image-converter.ts
+++ src/image-converter.ts
@@ -5,51 +5,53 @@ import * as path from "path";
 
 const SUPPORTED_FORMATS = [".jpg", ".jpeg", ".png"];
 
-const convertImage = async (inputFilePath: string, outputFilePath: string, texconvPath: string): Promise<void> => {
-    const tempFilePath = path.join(path.dirname(outputFilePath), `${path.parse(inputFilePath).name}.tmp.png`);
-    const inputImage = await Image.load(inputFilePath);
-    await inputImage.save(tempFilePath);
-    const outputDirPath = path.dirname(outputFilePath);
-    if (!existsSync(outputDirPath)) {
-        mkdirSync(outputDirPath, { recursive: true });
-    }
-    const args = ["-y", "-f", "DXT5", "-nologo", "-m", "1", "-o", outputDirPath, tempFilePath];
-    await execa(texconvPath, args);
-    // Delete temporary file
-    unlinkSync(tempFilePath);
-};
+class ImageConverter {
+    private inputFolderPath: string;
+    private outputFolderPath: string;
+    private texconvPath: string;
 
-const convertImageFile = async (
-    inputFile: string,
-    inputFolderPath: string,
-    outputFolderPath: string,
-    texconvPath: string
-): Promise<void> => {
-    const extension = path.extname(inputFile);
-
-    if (!SUPPORTED_FORMATS.includes(extension)) {
-        console.error(`Unsupported file format: ${inputFile}`);
-        return;
+    constructor(inputFolderPath: string, outputFolderPath: string, texconvPath: string) {
+        this.inputFolderPath = inputFolderPath;
+        this.outputFolderPath = outputFolderPath;
+        this.texconvPath = texconvPath;
     }
 
-    const inputFilePath = path.join(inputFolderPath, inputFile);
-    const outputFilePath = path.join(outputFolderPath, `${path.parse(inputFile).name}.dds`);
-    console.log(`Converting ${inputFilePath} to ${outputFilePath}`);
-
-    try {
-        await convertImage(inputFilePath, outputFilePath, texconvPath);
-    } catch (err) {
-        console.error(`Failed to convert ${inputFilePath}: ${err}`);
+    private async convertImage(inputFilePath: string, outputFilePath: string): Promise<void> {
+        const tempFilePath = path.join(path.dirname(outputFilePath), `${path.parse(inputFilePath).name}.tmp.png`);
+        const inputImage = await Image.load(inputFilePath);
+        await inputImage.save(tempFilePath);
+        const outputDirPath = path.dirname(outputFilePath);
+        if (!existsSync(outputDirPath)) {
+            mkdirSync(outputDirPath, { recursive: true });
+        }
+        const args = ["-y", "-f", "DXT5", "-nologo", "-m", "1", "-o", outputDirPath, tempFilePath];
+        await execa(this.texconvPath, args);
+        // Delete temporary file
+        unlinkSync(tempFilePath);
     }
-};
 
-const convertImageFiles = async (
-    files: string[],
-    inputFolderPath: string,
-    outputFolderPath: string,
-    texconvPath: string
-): Promise<void> => {
-    await Promise.all(files.map(file => convertImageFile(file, inputFolderPath, outputFolderPath, texconvPath)));
-};
+    public async convertImageFile(inputFile: string): Promise<void> {
+        const extension = path.extname(inputFile);
 
-export { convertImageFile, convertImageFiles };
+        if (!SUPPORTED_FORMATS.includes(extension)) {
+            console.error(`Unsupported file format: ${inputFile}`);
+            return;
+        }
+
+        const inputFilePath = path.join(this.inputFolderPath, inputFile);
+        const outputFilePath = path.join(this.outputFolderPath, `${path.parse(inputFile).name}.dds`);
+        console.log(`Converting ${inputFilePath} to ${outputFilePath}`);
+
+        try {
+            await this.convertImage(inputFilePath, outputFilePath);
+        } catch (err) {
+            console.error(`Failed to convert ${inputFilePath}: ${err}`);
+        }
+    }
+
+    public async convertImageFiles(files: string[]): Promise<void> {
+        await Promise.all(files.map(file => this.convertImageFile(file)));
+    }
+}
+
+export default ImageConverter;
--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,19 @@
 import { getMtaPath } from "./mta-installation";
 import { readdirSync, ensureDirSync } from "fs-extra";
 import * as path from "path";
-import { convertImageFiles } from "./image-converter";
+import ImageConverter from "./image-converter";
 
 const FOLDER_PATH = path.join(process.cwd(), "images");
 const FOLDER_PATH_OUTPUT = path.join(process.cwd(), "images_output");
 const TEXCONV_PATH = path.join(process.cwd(), "texconv.exe");
+const converter = new ImageConverter(FOLDER_PATH, FOLDER_PATH_OUTPUT, TEXCONV_PATH);
 
 (async function (): Promise<void> {
-    const files = readdirSync(FOLDER_PATH);
+    const inputFiles = readdirSync(FOLDER_PATH);
     try {
         await getMtaPath();
         ensureDirSync(FOLDER_PATH_OUTPUT);
-        convertImageFiles(files, FOLDER_PATH, FOLDER_PATH_OUTPUT, TEXCONV_PATH);
+        converter.convertImageFiles(inputFiles);
     } catch (err) {
         console.error(err);
     }
