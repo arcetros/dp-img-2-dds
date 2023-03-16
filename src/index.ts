eccfcee71a3874e3cac40647dc7084723da7f9f3
arcetros <75669794+arcetros@users.noreply.github.com>
2023-03-16 15:00:18 +0800

    refactor code to be more readable lol

--- src/index.ts
+++ src/index.ts
@@ -15,20 +15,18 @@ const converter = new ImageConverter(FOLDER_PATH, FOLDER_PATH_OUTPUT, TEXCONV_PA
 
 main();
 
-const exit = async () => {
-    inquirer
-        .prompt([
-            {
-                name: "moreQuery",
-                type: "confirm",
-                message: "Want to do anything else?"
-            }
-        ])
-        .then(answer => {
-            if (answer.moreQuery) return main();
-            return null;
-        });
-};
+async function exit() {
+    const { moreQuery } = await inquirer.prompt([
+        {
+            name: "moreQuery",
+            type: "confirm",
+            message: "Want to do anything else?"
+        }
+    ]);
+    if (moreQuery) {
+        await main();
+    }
+}
 
 async function main(): Promise<void> {
     const mtaPath = await getMtaPath();
@@ -46,94 +44,83 @@ async function main(): Promise<void> {
         clear();
         console.log(chalk.greenBright(figlet.textSync("dp-img-2-dds", { horizontalLayout: "full" })));
         console.log("");
-        inquirer.prompt(CONVERT_OPTIONS).then(({ convert_options }) => {
-            switch (convert_options) {
-                case 0:
-                    inquirer
-                        .prompt([
-                            {
-                                type: "checkbox",
-                                message: `Select up to ${inputFiles.length} files`,
-                                name: "options",
-                                choices: inputFiles
-                            }
-                        ])
-                        .then(({ options }) => converter.convertImageFiles(options).then(() => exit()));
-                    break;
-                case 1:
-                    converter.convertImageFiles(inputFiles).then(() => exit());
-                    break;
-                case 2:
-                    inquirer
-                        .prompt([
-                            {
-                                type: "checkbox",
-                                name: "options",
-                                message: `Select your converted image`,
-                                choices: outputDirents
-                                    .filter(dirent => {
-                                        return path.extname(dirent.name).toLowerCase() === ".dds";
-                                    })
-                                    .map(file => file.name),
-                                validate(target_dds) {
-                                    if (target_dds.length < 1) {
-                                        return "You have to select atleast one sticker";
-                                    }
-                                    return true;
-                                }
-                            }
-                        ])
-                        .then(({ options }: { options: string[] }) => {
-                            console.log("You have selected: ", options);
-                            inquirer
-                                .prompt([
-                                    {
-                                        type: "checkbox",
-                                        name: "replace_options",
-                                        message: `Select which original sticker you want to replace`,
-                                        choices: stickerDirents,
-                                        validate(target_dds) {
-                                            if (target_dds.length !== options.length) {
-                                                return `You must select exactly ${options.length} sticker${
-                                                    options.length === 1 ? "" : "s"
-                                                }`;
-                                            }
-                                            return true;
-                                        }
-                                    }
-                                ])
-                                .then(({ replace_options }: { replace_options: string[] }) => {
-                                    const filesToCopy = [];
-                                    for (let i = 0; i < options.length; i++) {
-                                        const obj = {
-                                            sourcePath: `${FOLDER_PATH_OUTPUT}/${options[i]}`,
-                                            targetPath:
-                                                i < replace_options.length
-                                                    ? `${originalStickerPath}/${replace_options[i]}`
-                                                    : null
-                                        };
-                                        filesToCopy.push(obj);
-                                    }
-                                    for (const file of filesToCopy) {
-                                        fs.unlink(file.targetPath);
+        const { convert_options } = await inquirer.prompt(CONVERT_OPTIONS);
+        switch (convert_options) {
+            case 0:
+                const { options } = await inquirer.prompt([
+                    {
+                        type: "checkbox",
+                        message: `Select up to ${inputFiles.length} files`,
+                        name: "options",
+                        choices: inputFiles
+                    }
+                ]);
+                await converter.convertImageFiles(options).then(() => exit());
+                break;
+            case 1:
+                converter.convertImageFiles(inputFiles).then(() => exit());
+                break;
+            case 2:
+                const targetDDSChoices = outputDirents
+                    .filter(dirent => path.extname(dirent.name).toLowerCase() === ".dds")
+                    .map(file => file.name);
 
-                                        fs.copyFile(file.sourcePath, file.targetPath, 1, err => {
-                                            if (err) {
-                                                console.log(err);
-                                            } else {
-                                                console.log(
-                                                    `File ${file.sourcePath} successfully copied and renamed to ${file.targetPath}.`
-                                                );
-                                            }
-                                        });
-                                    }
-                                });
-                        });
-                    break;
-                default:
-                    throw new Error(`Invalid option selected: ${convert_options}. Please select a valid option`);
-            }
-        });
+                const { select_options } = await inquirer.prompt([
+                    {
+                        type: "checkbox",
+                        name: "select_options",
+                        message: "Select your converted image",
+                        choices: targetDDSChoices,
+                        validate: targetDDS => {
+                            if (targetDDS.length < 1) {
+                                return "You have to select at least one sticker";
+                            }
+                            return true;
+                        }
+                    }
+                ]);
+
+                console.log("You have selected: ", select_options);
+
+                const { replace_options } = await inquirer.prompt([
+                    {
+                        type: "checkbox",
+                        name: "replace_options",
+                        message: `Select which original sticker you want to replace`,
+                        choices: stickerDirents,
+                        validate: selectedStickers => {
+                            if (selectedStickers.length !== select_options.length) {
+                                return `You must select exactly ${select_options.length} sticker${
+                                    select_options.length === 1 ? "" : "s"
+                                }`;
+                            }
+                            return true;
+                        }
+                    }
+                ]);
+                const filesToCopy = select_options.map((option, i) => ({
+                    sourcePath: `${FOLDER_PATH_OUTPUT}/${option}`,
+                    targetPath: `${originalStickerPath}/${replace_options[i]}`
+                }));
+
+                for (const file of filesToCopy) {
+                    try {
+                        await fs.promises.unlink(file.targetPath);
+                    } catch (error) {
+                        // ignore error if the file does not exist
+                    }
+
+                    try {
+                        await fs.promises.copyFile(file.sourcePath, file.targetPath);
+                        console.log(`File ${file.sourcePath} successfully copied and renamed to ${file.targetPath}.`);
+                    } catch (error) {
+                        console.log(`Error copying file from ${file.sourcePath} to ${file.targetPath}: ${error}`);
+                    }
+                }
+                break;
+            default:
+                throw new Error(`Invalid option selected: ${convert_options}. Please select a valid option`);
+        }
     } catch (err) {
         console.error(err);
     }
